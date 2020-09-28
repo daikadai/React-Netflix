52693a9faedb9ceb54f0beed1a266bcc6d77678d
Pham Cao Dai <daikadai1026@gmail.com>
2020-09-28 11:16:03 +0700

    8. Creating React Router Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import {FaqsContainer} from './containers/faqs';
-import {FooterContainer} from './containers/footer';
-import {JumbotronContainer} from './containers/jumbotron';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTER from './constants/routes';
+import { Browse, Home, Signin, Signup } from './pages';
 export default function App() {
   return (
-  <>
-    <JumbotronContainer />
-    <FaqsContainer />
-    <FooterContainer />
-  </>
+  <Router>
+      <Route exact path='/browse'>
+        <Browse />
+      </Route>
+      <Route exact path='/signin'>
+        <Signin />
+      </Route>
+      <Route exact path='/signup'>
+        <Signup/>
+      </Route>
+      <Route exact path={ROUTER.HOME}>
+        <Home />
+      </Route>
+  </Router>
   )
 }
 
---
+++ src/pages/signin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <p>Hello from Sign up</p>
+  )
+}
+
+export default SignIn
