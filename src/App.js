3720904445e75330ce1caf32fc72101924d75513
Pham Cao Dai <daikadai1026@gmail.com>
2020-09-29 16:46:46 +0700

    17. Creating an auth listener (custom hook)

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 import { Browse, Home, Signin, Signup } from "./pages";
+import { useAuthListener } from './hooks';
 
 export default function App() {
-  const user = null;
+  const {user} = useAuthListener();
+  console.log(user);
 
   return (
     <Router>
