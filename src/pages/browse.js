9a95920ad549fb0547043c68e53ea44011f834ca
Pham Cao Dai <daikadai1026@gmail.com>
2020-09-29 17:12:02 +0700

    18. Custom hook:browse page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useContent } from '../hooks'
 
 const Browse = () => {
+  // we need the series and the films
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  console.log(series);
+  console.log(films);
+  // we need slides
+  // pass it to the browser container
+
   return (
     <p>Hello from Browse</p>
   )
