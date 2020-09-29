86aa80aacd221b217a1a5a9e2e6d67e5fb37d4d6
Pham Cao Dai <daikadai1026@gmail.com>
2020-09-29 17:32:44 +0700

    19. Utils directory and setup

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { useContent } from '../hooks'
+import selectionFilter from '../utils/selection-filter';
 
 const Browse = () => {
   // we need the series and the films
   const { series } = useContent('series');
   const { films } = useContent('films');
-  console.log(series);
-  console.log(films);
   // we need slides
+  const slides = selectionFilter({ series, films });
   // pass it to the browser container
-
+  console.log(slides);
   return (
     <p>Hello from Browse</p>
   )
