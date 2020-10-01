3fee96ef1f13dc53c6c803f6b53089c2e268855c
Pham Cao Dai <daikadai1026@gmail.com>
2020-10-01 12:44:48 +0700

    27. Live search using Fuse.js

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Card, Header, Loading, Player } from "../components";
+import Fuse from 'fuse.js';
 import { FirebaseContext } from "../context/firebase";
 import { SelectProfileContainer } from "./profile";
 import * as ROUTES from "../constants/routes";
@@ -25,6 +26,19 @@ export function BrowseContainer({ slides }) {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ['data.description','data.title', 'data.genre']
+    });
+    const results = fuse.search(searchTerm).map(({item}) => item);
+
+    if(slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category])
+    }
+  }, [searchTerm])
 
   return profile.displayName ? (
     <>
